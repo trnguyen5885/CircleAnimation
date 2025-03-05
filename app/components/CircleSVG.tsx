b2e79d80cee91913b882f6115362cbf86fb81fea
Nguyễn Trần Trung Nguyên <macos@nguyentrantrungnguyenk4it.local>
2025-03-05 13:23:53 +0700

    Circle inner Rectangle

--- app/components/CircleSVG.tsx
+++ app/components/CircleSVG.tsx
@@ -4,11 +4,15 @@ import SVG, { Circle, Rect } from "react-native-svg";
 
 const { width, height } = Dimensions.get("window");
 
-const widthFrame = 600;
+const widthFrame = 300;
 
 const CircleSVG = () => {
     const widthRect = widthFrame * 0.5;
     const locationRect = (widthFrame - widthRect) / 2;
+    const strokeWidthRect = 2;
+    const strokeWidthCircle = 0;
+    const originR = widthRect / 2;
+    const afterR = originR - strokeWidthRect / 2 - strokeWidthCircle / 2;
 
     return (
         <View style={styles.container}>
@@ -23,17 +27,34 @@ const CircleSVG = () => {
                     width={widthRect}
                     height={widthRect}
                     stroke="red"
-                    strokeWidth="3"
+                    strokeWidth={strokeWidthRect}
                     fill="transparent"
                 />
+                {/* <Rect
+                    x={locationRect}
+                    y={locationRect}
+                    width={widthRect}
+                    height={widthRect}
+                    stroke="black"
+                    strokeWidth="1"
+                    fill="transparent"
+                /> */}
                 <Circle
                     cx={widthRect / 2 + locationRect}
                     cy={widthRect / 2 + locationRect}
-                    r={widthRect / 2}
+                    r={afterR}
                     stroke="blue"
-                    strokeWidth="2.5"
+                    strokeWidth={strokeWidthCircle}
                     fill="green"
                 />
+                {/* <Circle
+                    cx={widthRect / 2 + locationRect}
+                    cy={widthRect / 2 + locationRect}
+                    r={afterR}
+                    stroke="black"
+                    strokeWidth="1"
+                    fill="transparent"
+                /> */}
             </SVG>
         </View>
     );
@@ -46,6 +67,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        transform: [
+            {
+                scale: 1.8,
+            },
+        ],
     },
     containerSVG: {
         borderWidth: 1,
